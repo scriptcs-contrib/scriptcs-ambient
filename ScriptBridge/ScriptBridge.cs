ad1e1fbfb5ed3e666c50d63798595a4483fb300f
Stephen Nuchia <snuchia@gmail.com>
2016-07-21 01:46:04 -0500

    basic strongly ambient logic working

---
+++ ScriptBridge/ScriptBridge.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptCs;
+using ScriptCs.Contracts;
+using ScriptCs.Engine.Roslyn;
+using ScriptCs.Hosting;
+
+namespace ScriptBridge
+{
+    public class MY_ScriptHost : ScriptHost
+    {
+        public MY_ScriptHost(IScriptPackManager scriptPackManager, ScriptEnvironment environment)
+            : base(scriptPackManager, environment)
+        {
+        }
+
+        public dynamic InputAmbient { get; set; }
+        public dynamic OutputAmbient { get; set; }
+    }
+
+    class MY_Factory : IScriptHostFactory
+    {
+        Dictionary<string, object> m_globs;
+        public MY_ScriptHost m_host;
+        public MY_Factory(Dictionary<string, object> globs)
+        {
+            m_globs = globs;
+        }
+        public IScriptHost CreateScriptHost(IScriptPackManager scriptPackManager, string[] scriptArgs)
+        {
+            m_host = new MY_ScriptHost(scriptPackManager, new ScriptEnvironment(scriptArgs));
+            m_host.InputAmbient = m_globs["InputAmbient"];
+            m_host.OutputAmbient = m_globs["OutputAmbient"];
+            return m_host;
+        }
+    }
+    public class ScriptBridge
+    {
+        Dictionary<string, object> m_globals;
+        public ScriptBridge(Dictionary<string,object> Globals)
+        {
+            m_globals = Globals;
+        }
+
+        public dynamic Execute(string Code)
+        {
+            var console = (IConsole)new ScriptConsole();
+            var logProvider = new ColoredConsoleLogProvider(LogLevel.Info, console);
+
+            var builder = new ScriptServicesBuilder(console, logProvider);
+            builder.ScriptHostFactory<MY_Factory>();
+            builder.LogLevel(LogLevel.Info).Cache(false).Repl(false).ScriptEngine<CSharpScriptEngine>();
+
+            var shf = new MY_Factory(m_globals);
+            builder.SetOverride<IScriptHostFactory, MY_Factory>(shf);
+            var services = builder.Build();
+
+            var executor = (ScriptExecutor)services.Executor;
+            executor.Initialize(Enumerable.Empty<string>(), Enumerable.Empty<IScriptPack>());
+
+            var result = executor.ExecuteScript(Code);
+            m_globals["OutputAmbient"] = shf.m_host.OutputAmbient;
+
+            if (result.CompileExceptionInfo != null)
+                throw result.CompileExceptionInfo.SourceException;
+            if (result.ExecuteExceptionInfo != null)
+                throw result.ExecuteExceptionInfo.SourceException;
+
+            return result.ReturnValue;
+        }
+    }
+}
---
+++ scriptcsambient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptBridge;
+
+namespace scriptcsambient
+{
+    public class InputThing
+    {
+        public string ThingString { get; set; }
+    }
+
+    public class OutputThing
+    {
+        public string OutThingString { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var it = new InputThing();
+            var ot = new OutputThing();
+            it.ThingString = "Random Input";
+            var globs = new Dictionary<string, object>();
+            globs.Add("InputAmbient", it);
+            globs.Add("OutputAmbient", ot);
+            var sb = new ScriptBridge.ScriptBridge(globs);
+            var code = "OutputAmbient.OutThingString = InputAmbient.ThingString + \" (Out)\";";
+            sb.Execute(code);
+            Console.WriteLine(ot.OutThingString);
+        }
+    }
+}
